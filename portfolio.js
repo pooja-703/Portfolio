a22473d308fdbf0b9f45ba4d87dff8e3f11a0062
Pooja Chavda <poojachavda010@gmail.com>
2025-02-15 09:09:03 +0530

    Initial commit

---
+++ portfolio.js
@@ -0,0 +1,18 @@
+// // sidebar 
+// function open1() {
+//     document.getElementById("sidebar").style.display = "block"
+//     document.getElementById("menubtn").style.display = "none"
+// }
+
+// function close1() {
+//     document.getElementById("sidebar").style.display = "none"
+//     document.getElementById("menubtn").style.display = "block"
+// }
+
+// function home1() {
+//     document.getElementById("sidebar").style.display = "none"
+//     document.getElementById("menubtn").style.display = "block"
+// }
+
+// welcome to this page form G_Harsh.
+// I have created this porject ,so if you like this page then give a review about this to me
